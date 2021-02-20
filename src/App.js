5afa0a6629ce9555dcb22813ab8c09cfa76bd64b
yuukiasuna00 <yuukixasuna00@gmail.com>
2021-02-20 16:40:49 -0300

    style: added main style

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
-import { useState } from "react";
-import { GlobalStyle } from "./globalStyle";
+import { useState } from 'react';
+import { GlobalStyle } from './globalStyle';
 import RaindayVid from './video/rainday-vid.mp4';
 
+import { WeatherContainer } from './WeatherStyle';
+
 const api = {
-  base: "https://api.openweathermap.org/data/2.5/",
+  base: 'https://api.openweathermap.org/data/2.5/',
 };
 
 const API_KEY = process.env.REACT_APP_KEY;
 
 function App() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
   const search = (event) => {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${API_KEY}`)
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
-          setQuery("");
+          setQuery('');
         });
     }
   };
 
   const dateBuilder = (d) => {
     let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
     ];
     let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
     ];
 
     let day = days[d.getDay()];
@@ -60,35 +62,37 @@ function App() {
     <>
       <div>
         <div>
-          <video src={RaindayVid} autoPlay muted loop/>
+          {/* <video src={RaindayVid} autoPlay muted loop /> */}
         </div>
-        <main>
-          <div>
-            <input
-              type="text"
-              placeholder="City..."
-              onChange={(e) => setQuery(e.target.value)}
-              value={query}
-              onKeyPress={search}
-            />
-          </div>
-          {typeof weather.main !== "undefined" ? (
-            <>
-              <div>
-                <h2>
-                  {weather.name}, {weather.sys.country}
-                </h2>
-                <h4>{dateBuilder(new Date())}</h4>
-              </div>
-              <div>
-                <p>{Math.round(weather.main.temp)}&deg;c</p>
-                <p>{weather.weather[0].main}</p>
-              </div>
-            </>
-          ) : (
-            ""
-          )}
-        </main>
+        <WeatherContainer>
+          <main>
+            <div className='search-content'>
+              <input
+                type="text"
+                placeholder="City..."
+                onChange={(e) => setQuery(e.target.value)}
+                value={query}
+                onKeyPress={search}
+              />
+            </div>
+            {typeof weather.main !== 'undefined' ? (
+              <>
+                <div>
+                  <h2>
+                    {weather.name}, {weather.sys.country}
+                  </h2>
+                  <h4>{dateBuilder(new Date())}</h4>
+                </div>
+                <div>
+                  <p>{Math.round(weather.main.temp)}&deg;c</p>
+                  <p>{weather.weather[0].main}</p>
+                </div>
+              </>
+            ) : (
+              ''
+            )}
+          </main>
+        </WeatherContainer>
       </div>
       <GlobalStyle />
     </>
--- src/WeatherStyle.js
+++ src/WeatherStyle.js
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const WeatherContainer = styled.div``
+export const WeatherContainer = styled.div`
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
