2b1890724a8521ecbbbf7bc4a5798c9e4dc74ee9
yuukiasuna00 <yuukixasuna00@gmail.com>
2021-02-20 15:48:36 -0300

    fix: removed import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { GlobalStyle } from "./globalStyle";
 
-import "weather-icons/css/weather-icons.css";
 
 const api = {
   key: "9d7ad32e1bb9ab58ca26241ea1d81bfc",
