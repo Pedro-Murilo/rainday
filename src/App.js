2c86c8340fb696d9b9d5cdc239071ea3368946aa
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-02-20 14:38:25 -0300

    feat: feature to find the weather with openweatherapi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,93 @@
-import { GlobalStyle } from './globalStyle';
+import { useState } from "react";
+import { GlobalStyle } from "./globalStyle";
+
+import "weather-icons/css/weather-icons.css";
+
+const api = {
+  key: "9d7ad32e1bb9ab58ca26241ea1d81bfc",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (event) => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
     <>
-    <div className="App">
-      Hello World
-    </div>
-    <GlobalStyle />
+      <div>
+        <main>
+          <div className="search-box">
+            <input
+              type="text"
+              placeholder="City..."
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+            />
+          </div>
+          {typeof weather.main !== "undefined" ? (
+            <>
+              <div className="location-box">
+                <div className="locaiton">
+                  {weather.name}, {weather.sys.country}
+                </div>
+                <div className="date">{dateBuilder(new Date())}</div>
+              </div>
+              <div className="weather-box">
+                <div className="temp">{Math.round(weather.main.temp)}&deg;c</div>
+                <div>{weather.weather[0].main}</div>
+              </div>
+            </>
+          ) : (
+            ""
+          )}
+        </main>
+      </div>
+      <GlobalStyle />
     </>
   );
 }
